b5c9e6c759adf78005553e0f093fbeb6ef2771df
lora-git <lora_git@protonmail.com>
2025-03-14 12:48:05 -0700

    updated fps in main.py

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from constants import *
 def main():
    pygame.init()
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   clock = pygame.time.Clock() # Create clock obj
+   dt = 0 # Initialize detla time var
    
    print("Starting Asteroids!")
    print(f"Screen width: {SCREEN_WIDTH}")
    print(f"Screen height: {SCREEN_HEIGHT}")
-
+   
    while True:
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
             return
-         
-      # Fill the screen with black
-      screen.fill((0, 0, 0))
+            
+      screen.fill((0, 0, 0)) # Fill the screen with black
 
       # Refresh the screen
       pygame.display.flip()
+   
+      dt = clock.tick(60) / 1000  # Update dt
 
    pygame.quit()
 
